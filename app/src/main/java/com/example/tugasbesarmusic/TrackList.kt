7ea8fc9af95b00175d651c8573ac455d47881f53
saka <saka31.sw@gmail.com>
2022-11-26 13:35:12 +0700

    Menambahkan bagian atas tracklist, menambah fungsi button back tracklist. Menghilangkan action BAR.

--- app/src/main/java/com/example/tugasbesarmusic/TrackList.kt
+++ app/src/main/java/com/example/tugasbesarmusic/TrackList.kt
@@ -1,14 +1,17 @@
 package com.example.tugasbesarmusic
 
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_tracklist.*
 
 class TrackList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,19 @@ class TrackList : AppCompatActivity() {
         setContentView(R.layout.activity_tracklist)
 
         imageAdapter()
-
+        back_tracklist()
+    }
+    private fun back_tracklist(){
+        tombolback_tracklist.setOnClickListener(
+            View.OnClickListener {
+                /*val i = Intent(
+                   this,
+                   halaman2::class.java)
+                startActivity(i)
+                BACK dicomment karena belum ada file
+                xml yang menuju ke halaman tsb belum di push oleh rekan sekelompok */
+            }
+        )
     }
 
     private fun imageAdapter(){
