d56c7a842b0dd58b18961bb740aa5810f13e993d
Adrian Reyhan <adrianreyhan03.ar@gmail.com>
2022-11-28 09:29:02 +0700

    merapikan

--- app/src/main/java/com/example/tugasbesarmusic/IsiPlaylist.kt
+++ app/src/main/java/com/example/tugasbesarmusic/IsiPlaylist.kt
@@ -2,12 +2,33 @@ package com.example.tugasbesarmusic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class IsiPlaylist: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_isi_playlist)
 
+        modelAdapterIsiPlaylist()
+    }
+    private fun modelAdapterIsiPlaylist(){
+        val isiPlaylist = listOf<ModelTrackList>(
+            ModelTrackList(1, "Cinta Butuh Waktu", "Vierratele", R.drawable.viera),
+            ModelTrackList(2, "Seandainya", "Vierratele", R.drawable.vieera),
+            ModelTrackList(3, "Perih", "Vierratele", R.drawable.vieraaa),
+            ModelTrackList(4, "Dengarkan Curhatku", "Vierratele", R.drawable.vierra),
+            ModelTrackList(5, "Rasa Ini", "Vierratele", R.drawable.vierrraa),
+            ModelTrackList(6, "Tinggalkanku", "Vierratele", R.drawable.cover_white),
+            ModelTrackList(7, "Nama Track", "Nama Artist", R.drawable.cover_white),
+            ModelTrackList(8, "Nama Track", "Nama Artist", R.drawable.cover_white),
+            ModelTrackList(9, "Nama Track", "Nama Artist", R.drawable.cover_white),
+            ModelTrackList(10, "Nama Track", "Nama Artist", R.drawable.cover_white),
+            ModelTrackList(11, "Nama Track", "Nama Artist", R.drawable.cover_white),
+            ModelTrackList(12, "Nama Track", "Nama Artist", R.drawable.cover_white),
+        )
+
+        val imageAdapterIsiPlaylist = ImageAdapterTracklist( isiPlaylist )
+        findViewById<RecyclerView>(R.id.recyclerView).adapter = imageAdapterIsiPlaylist
     }
 
     }
