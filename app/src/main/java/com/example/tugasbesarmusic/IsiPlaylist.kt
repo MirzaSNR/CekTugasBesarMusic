be97e8fbce7224613cfcc95511644492292edb61
Adrian Reyhan <adrianreyhan03.ar@gmail.com>
2022-11-28 08:09:58 +0700

    merapikan

--- app/src/main/java/com/example/tugasbesarmusic/IsiPlaylist.kt
+++ app/src/main/java/com/example/tugasbesarmusic/IsiPlaylist.kt
@@ -8,25 +8,6 @@ class IsiPlaylist: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_isi_playlist)
 
-        modelAdapterIsiPlaylist()
-
-
     }
 
-    private fun modelAdapterIsiPlaylist() {
-        val tracklist = listOf<ModelTrackList>(
-            ModelTrackList(1, "Cinta Butuh Waktu", "Vierratele", R.drawable.vierrraa),
-            ModelTrackList(2, "Seandainya", "Vierratele", R.drawable.vierra),
-            ModelTrackList(3, "Perih", "Vierratele", R.drawable.vieraaa),
-            ModelTrackList(4, "Dengarkan Curhatku", "Vierratele", R.drawable.vieera),
-            ModelTrackList(5, "Rasa Ini", "Vierratele", R.drawable.viera),
-            ModelTrackList(6, "Tinggalkanku", "Vierratele", R.drawable.cover_white),
-            ModelTrackList(7, "Nama Track", "Nama Artist", R.drawable.cover_white),
-            ModelTrackList(8, "Nama Track", "Nama Artist", R.drawable.cover_white),
-            ModelTrackList(9, "Nama Track", "Nama Artist", R.drawable.cover_white),
-            ModelTrackList(10, "Nama Track", "Nama Artist", R.drawable.cover_white),
-            ModelTrackList(11, "Nama Track", "Nama Artist", R.drawable.cover_white),
-            ModelTrackList(12, "Nama Track", "Nama Artist", R.drawable.cover_white),
-        )
     }
-}
