5abf9f9b68ca143b56d4b10b54537fbceff08020
Sofiyan Sofiyan <lisgiyantosofiyan@gmail.com>
2022-11-30 22:46:45 +0700

    luurr, saya membuat fungsi tombol tambahan pada pilihan, albumlist,folderlist,tracklist, dan playlist. (sudah saya sesuaikan dan arahkan ke activity pada masing" project teman-teman satu suku. :)

--- app/src/main/java/com/example/tugasbesarmusic/Mylibrary.kt
+++ app/src/main/java/com/example/tugasbesarmusic/Mylibrary.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.RelativeLayout
 
 class Mylibrary : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,33 @@ class Mylibrary : AppCompatActivity(){
 
         val clickback = findViewById<ImageView>(R.id.back_icon)
         clickback.setOnClickListener(){
-            val intent = Intent(this, IsiPlaylist::class.java)
+            val intent = Intent(this@Mylibrary, HomeMusic::class.java)
             startActivity(intent)
         }
+
+        val albumlist = findViewById<RelativeLayout>(R.id.albumlist)
+        albumlist.setOnClickListener(){
+            val intent = Intent(this@Mylibrary, MusicAlbum::class.java)
+            startActivity(intent)
+        }
+
+        val tracklist = findViewById<RelativeLayout>(R.id.tracklist)
+        tracklist.setOnClickListener(){
+            val intent = Intent(this@Mylibrary, TrackList::class.java)
+            startActivity(intent)
+        }
+
+        val folderList = findViewById<RelativeLayout>(R.id.folderlist)
+        folderList.setOnClickListener(){
+            val intent = Intent(this@Mylibrary, FolderList::class.java)
+            startActivity(intent)
+        }
+
+        val playlist = findViewById<RelativeLayout>(R.id.playlist)
+        playlist.setOnClickListener(){
+            val intent = Intent(this@Mylibrary, IsiPlaylist::class.java)
+            startActivity(intent)
+        }
+
     }
 }
