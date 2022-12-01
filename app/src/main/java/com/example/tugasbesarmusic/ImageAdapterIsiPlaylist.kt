fa36e6b63323375d036fb5a6bcf0858b2133059b
sunny-smiles <majidfikri10@gmail.com>
2022-12-01 13:18:26 +0700

    ini reyhan membenarkan di laptop majid

--- app/src/main/java/com/example/tugasbesarmusic/ImageAdapterIsiPlaylist.kt
+++ app/src/main/java/com/example/tugasbesarmusic/ImageAdapterIsiPlaylist.kt
@@ -12,21 +12,21 @@ class ImageAdapterIsiPlaylist (
 ) : RecyclerView.Adapter<ImageAdapterIsiPlaylist.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.adapter_image_tracklist, parent, false)
+        LayoutInflater.from(parent.context).inflate(R.layout.adapter_image_isiplaylist, parent, false)
     )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val tracklist = listIsiPlaylist[position]
-        holder.imageView.setImageResource( tracklist.image )
-        holder.textView.text = tracklist.judul
-        holder.textView2.text = tracklist.pencipta
+        val isiPlaylist = listIsiPlaylist[position]
+        holder.imageView.setImageResource( isiPlaylist.image )
+        holder.textView_isiplaylist.text = isiPlaylist.judul
+        holder.textView2_isiplaylist.text = isiPlaylist.pencipta
     }
 
     override fun getItemCount() = listIsiPlaylist.size
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val imageView = view.findViewById<ImageView>(R.id.imageView)
-        val textView = view.findViewById<TextView>(R.id.textView)
-        val textView2 = view.findViewById<TextView>(R.id.textView2)
+        val textView_isiplaylist = view.findViewById<TextView>(R.id.textView_isiplaylist)
+        val textView2_isiplaylist = view.findViewById<TextView>(R.id.textView2_isiplaylist)
     }
 }
