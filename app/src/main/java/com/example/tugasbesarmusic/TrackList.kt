0ddef2068fee2097452ac6ebe33d8fcc97ad0ee0
saka <saka31.sw@gmail.com>
2022-12-04 07:19:06 +0700

    Memperbaiki Fungsi Searchview ke tempat sesuai contoh layout yang diberikan

--- app/src/main/java/com/example/tugasbesarmusic/TrackList.kt
+++ app/src/main/java/com/example/tugasbesarmusic/TrackList.kt
@@ -48,6 +48,7 @@ class TrackList : AppCompatActivity() {
         imageAdapterTracklist.notifyDataSetChanged()
     }
 
+    //Fungsi Button Back
     private fun back_tracklist(){
         tombolback_tracklist.setOnClickListener(
 
@@ -60,29 +61,17 @@ class TrackList : AppCompatActivity() {
         )
     }
 
+    //Fungsi Search
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // below line is to get our inflater
-        val inflater = menuInflater
 
-        // inside inflater we are inflating our menu file.
-        inflater.inflate(R.menu.search_menu_tracklist, menu)
-
-        // below line is to get our menu item.
-        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
-
-        // getting search view of our item.
-        val searchView: SearchView = searchItem.getActionView() as SearchView
-
-        // below line is to call set on query text listener method.
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+        btnsearch_tracklist.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
             android.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(p0: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(msg: String): Boolean {
-                // inside on query text change method we are
-                // calling a method to filter our recycler view.
+
                 filter(msg)
                 return false
             }
@@ -90,26 +79,24 @@ class TrackList : AppCompatActivity() {
         return true
     }
 
+    //filter digunakan untuk Fungsi Search
     private fun filter(text: String) {
-        // creating a new array list to filter our data.
+
         val filteredlist: ArrayList<TracklistAttribute> = ArrayList()
 
-        // running a for loop to compare elements.
+
         for (item in courseList) {
-            // checking if the entered string matched with any item of our recycler view.
+
             if (item.title.toLowerCase().contains(text.toLowerCase())) {
-                // if the item is matched we are
-                // adding it to our filtered list.
+
                 filteredlist.add(item)
             }
         }
         if (filteredlist.isEmpty()) {
-            // if no item is added in filtered list we are
-            // displaying a toast message as no data found.
+
             Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show()
         } else {
-            // at last we are passing that filtered
-            // list to our adapter class.
+
             imageAdapterTracklist.filterList(filteredlist)
         }
     }
