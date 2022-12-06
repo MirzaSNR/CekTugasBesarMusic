d0ffc32ae8143330777e372f82aa3e6d948f56f0
MirzaSNR <mirza04112003@gmail.com>
2022-12-06 13:13:48 +0700

    Menjalankan Tombol Back dan Repeat Di Home Music

--- app/src/main/java/com/example/tugasbesarmusic/HomeMusic.kt
+++ app/src/main/java/com/example/tugasbesarmusic/HomeMusic.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_home_music.*
 
 class HomeMusic : AppCompatActivity() {
+    var repeatsatu :Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_music)
@@ -15,5 +17,21 @@ class HomeMusic : AppCompatActivity() {
         ibtn1.setOnClickListener {
             startActivity(Intent(this,TrackList::class.java)   )
         }
+        ibtnsetting.setOnClickListener {
+            val intent:Intent = Intent(this, SettingAplikasi::class.java)
+            startActivity(intent)
+        }
+        btnrepeat_HomeMusic.setOnClickListener {
+            if (!repeatsatu){
+                repeatsatu = true
+                btnrepeat_HomeMusic.setBackgroundResource(R.drawable.repeat)
+                Toast.makeText(this,"Repeat Satu", Toast.LENGTH_SHORT).show()
+            }else{
+                repeatsatu = false
+                btnrepeat_HomeMusic.setBackgroundResource(R.drawable.repeat)
+                Toast.makeText(this,"Repeat Semua", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
