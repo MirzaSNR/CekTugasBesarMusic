13965eb56283274d7cadcbbe8b4f68b0fa81a7c9
nizamiki60 <nizamiki60@gmail.com>
2022-12-08 22:54:09 +0700

    fungsi playmusic play,prev,next,seekbar tapi masih eror

--- app/src/main/java/com/example/tugasbesarmusic/HomeMusic.kt
+++ app/src/main/java/com/example/tugasbesarmusic/HomeMusic.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.SeekBar
 import android.widget.Toast
+import androidx.core.util.TimeUtils.formatDuration
 import kotlinx.android.synthetic.main.activity_home_music.*
 
 class HomeMusic : AppCompatActivity() {
@@ -31,7 +33,43 @@ class HomeMusic : AppCompatActivity() {
                 btnrepeat_HomeMusic.setBackgroundResource(R.drawable.repeat)
                 Toast.makeText(this,"Repeat Semua", Toast.LENGTH_SHORT).show()
             }
-        }
+            }
 
-    }
-}
+         }
+      //     try {
+        //       val bundle: Bundle? = intent.extras
+
+          //     if (bundle != null) {
+            //       initializeLayout(bundle)
+                }
+        // seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+          //   override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            //     if (p2) {
+              //       mediaPlayer!!.seekTo(p1)
+        //             start.text = formatDuration(mediaPlayer!!.currentPosition.toLong())
+               //  }
+             // }
+
+           //  override fun onStartTrackingTouch(p0: SeekBar?) {
+
+             // }
+
+            // override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            // }
+
+   // }
+     //       catch (e: Exception) {
+           //     Toast.makeText(this, "Error" + e.message, Toast.LENGTH_SHORT).show()
+            //}
+
+       //     next.setOnClickListener { prevNextSong(increment = true) }
+         //   fab_prev.setOnClickListener { prevNextSong(increment = false) }
+          //  fab_play.setOnClickListener {
+            //    if (isPlaying)
+              //      pause()
+              //  else
+                //    play()
+           // }
+   // }
+// }
